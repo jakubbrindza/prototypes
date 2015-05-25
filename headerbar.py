f00007087857aa55c71cea14b2a9fbbba9cc8ec4
jakubbrindza <jakub.brindza@gmail.com>
2015-05-25 07:55:11 +0100

    HB python

--- headerbar.py
+++ headerbar.py
@@ -7,22 +7,29 @@ class HeadeBarApplication(Gtk.Application):
    	def _init_signal_connections(self):
    		'''connect signals on UI elements in headerbar.ui'''
 
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file("headerbar.ui")
+		        
+
    		SIGNAL_CONNECTIONS_DIC = {
    		"on_add_task": self.on_add_task
    		}
 
-   		builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
+   		self.builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
+
+		self.window = builder.get_object("MainWindow")
+		self.window.show_all
+
+		Gtk.main()
+
 
    	def on_add_task(self, button):
    		print("Success!")
 
 
-builder = Gtk.Builder()
-builder.add_from_file("headerbar.ui")
-        
-window = builder.get_object("MainWindow")
-window.show_all
+
+
 		
-Gtk.main()
+
 
 
