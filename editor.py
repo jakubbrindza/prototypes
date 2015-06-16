f55efd3d8a5ebe0c1f17f86a977256dad92ed1b5
jakubbrindza <jakub.brindza@gmail.com>
2015-06-16 10:19:08 +0100

    editor

--- editor.py
+++ editor.py
@@ -2,38 +2,39 @@ import sys
 
 from gi.repository import Gtk, Gio
 
-class Editorlication(Gtk.Application):
+class EditorApplication(Gtk.Application):
+   def __init__(self):
+      self._init_signal_connections()
 
-   	def _init_signal_connections(self):
-   		'''connect signals on UI elements in headerbar.ui'''
+   def _init_signal_connections(self):
 
-		self.builder = Gtk.Builder()
-		self.builder.add_from_file("editor.ui")
-		        
+      self.builder = Gtk.Builder()
+      self.builder.add_from_file("editor.ui")
 
-   		SIGNAL_CONNECTIONS_DIC = {
-   		"on_mark_as_done": self.on_mark_as_done,
-         "on_dismiss": self.on_dismiss
-   		}
+      SIGNAL_CONNECTIONS_DIC = {
+      "on_mark_as_done": self.on_mark_as_done,
+      "on_dismiss": self.on_dismiss
+      }
 
-   		self.builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
+      self.builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
 
-		self.window = builder.get_object("MainWindow")
-		self.window.show_all
+      self.window = builder.get_object("MainWindow")
+      self.window.show_all()
 
-		Gtk.main()
+   def on_mark_as_done(self, button):
+      print("Task marked as done")
 
+   def on_dismiss(self, button):
+      print("Task dismissed")
 
-   	def on_mark_as_done(self, button):
-   		print("Task marked as done")
-
-      def on_dismiss(self, button):
-         print("Task dismissed")
-
+win = EditorApplication().window
+win.connect("delete-event", Gtk.main_quit)
+Gtk.main()
 
 
 
-		
+
+
 
 
 
